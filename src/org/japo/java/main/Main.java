235f91b6fd51b3b4db5c878499eb923ea9825c2e
pablomunmel <pabmunmel@gmail.com>
2023-09-27 18:51:04 +0200

    Concatenación de variable :recycle:

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -25,8 +25,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola Mundo");
+        String nombre = "Pablo Munera Mellado";
+        System.out.println("Hola " + nombre);
     }
     
 }
